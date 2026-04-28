81db92c749d7a8daf1de63d9f3c60d89837bb5bc
Mohamed_Safnas <s23010675@ousl.lk>
2026-04-28 17:50:07 +0530

    fix add-product route

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 // test route
 app.get("/", (req, res) => {
   res.send("POS Cloud Running");
+});
+
+// ADD PRODUCT ROUTE (THIS IS REQUIRED)
+app.post("/add-product", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    message: "Product received",
+    product: req.body
+  });
 });
 
 const PORT = process.env.PORT || 5000;
